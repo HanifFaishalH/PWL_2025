0ed3577f5e37535edf035a0a536326e115879a80
Hanif Faishal Hilmi <haniffaishalh@gmail.com>
2025-02-19 10:15:30 +0700

    add: controller (Welcome controller already pushed)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,9 +22,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return 'Hello World';
-});
+// Route::get('/hello', function () {
+//     return 'Hello World';
+// });
 
 Route::get('/world', function() {
     return 'world';
@@ -29,10 +34,10 @@ Route::get('/welcome', function() {
     return 'selamat datang';
 });
 
-Route::get('/about', function() {
-    return "NIM     : 2341720116
-    Nama    : Hanif Faishal Hilmi";
-});
+// Route::get('/about', function() {
+//     return "NIM     : 2341720116
+//     Nama    : Hanif Faishal Hilmi";
+// });
 
 Route::get('/user/{name}', function($name) {
     return 'Nama saya '.$name;
@@ -42,6 +47,35 @@ Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId) {
     return 'Pos ke-'.$postId." Komentar ke- ".$commentId;
 });
 
-Route::get('/articles/{id}', function($id) {
-    return 'Halaman artikel ke-'.$id;
-});
+// Route::get('/articles/{id}', function($id) {
+//     return 'Halaman artikel ke-'.$id;
+// });
+
+// controller
+
+Route::get('/hello', [WelcomeController::class, 'hello']);
+
+
+// Route::get('/', [PageController::class, 'index']);
+// Route::get('/about', [PageController::class, 'about']);
+// Route::get('/articles/{id}', [PageController::class, 'articles']);
+
+//single controller
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'about']);
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
+//resource controller
+
+use App\Http\Controllers\PhotoController;
+
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]); 
