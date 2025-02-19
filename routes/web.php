ef5c2b989de1ee83223211ae8cd14f12b2147711
Hanif Faishal Hilmi <haniffaishalh@gmail.com>
2025-02-19 09:15:05 +0700

    add : learn route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function() {
+    return 'world';
+});
+
+Route::get('/welcome', function() {
+    return 'selamat datang';
+});
+
+Route::get('/about', function() {
+    return "NIM     : 2341720116
+    Nama    : Hanif Faishal Hilmi";
+});
+
+Route::get('/user/{name}', function($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke- ".$commentId;
+});
+
+Route::get('/articles/{id}', function($id) {
+    return 'Halaman artikel ke-'.$id;
+});
